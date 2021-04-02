0ad3b764a6bf9402d6811a8dd7f60ae27a0c3c5c
loclv <masamonedante@gmail.com>
2021-04-02 15:42:40 +0700

    add igx pie chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ie-igniteui-angular-charts-demo';
+
+  data = [
+    { MarketShare: 30, Company: 'Google' },
+    { MarketShare: 30, Company: 'Apple' },
+    { MarketShare: 15, Company: 'Microsoft' },
+    { MarketShare: 15, Company: 'Samsung' },
+    { MarketShare: 10, Company: 'Other' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IgxPieChartModule } from 'igniteui-angular-charts';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, IgxPieChartModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
